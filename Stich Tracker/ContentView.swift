bc03795aad0b9443f91c90bb911055ed7b6e643b
Hoang Nguyen <hoang.nguyen@xing.com>
2020-01-21 09:42:00 +0100

    Initial Commit

---
+++ Stich Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stich Tracker
+//
+//  Created by Hoang Nguyen on 21.01.20.
+//  Copyright © 2020 Hoang Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
